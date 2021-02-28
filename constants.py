742782a0a8d787b87f30a12271d7cad02bf82124
Davide Santambrogio <dsantamb@amazon.com>
2021-02-28 10:18:04 +0100

    colab data path

--- constants.py
+++ constants.py
@@ -1,6 +1,12 @@
 import torch
 
-DATA_ROOT = './data/'
+colab = True
+
+if colab:
+    DATA_ROOT = '/content/gdrive/MyDrive/Colab_Notebooks/data/'
+else:
+    DATA_ROOT = './data/'
+    
 MODELS_ROOT = './models/'
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
