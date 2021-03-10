c0fc4b0516129d10dc85afb8a93c1da035eed728
alessandro.paticchio <alessandro.paticchio@moviri.com>
2021-03-10 22:13:43 +0100

    fix on latent space

--- visualize_latent_space.py
+++ visualize_latent_space.py
@@ -5,38 +5,40 @@ import matplotlib.pyplot as plt
 import torch
 
 batch_size = 1
-dataset = 'rays'
+dataset = 'potential'
 
-train_dataset = torch.load(DATA_ROOT + 'real_data/' + dataset + '_pic_data/training_' + dataset + '.pt')
+train_dataset = torch.load(DATA_ROOT + 'DATA21.2.18/loaded_data/' + 'training_' + dataset + '.pt')
 train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
 
-model_name = 'rays_VAE_2020-12-21 11:16:48.097754.pt'
+model_name = 'potential_VAE__2021-03-10 20_41_23.106999.pt'
 model_path = MODELS_ROOT + model_name
 
 if dataset == 'rays':
     image_size = RAYS_IMAGE_SIZE
+    image_channels = RAYS_IMAGE_CHANNELS
     hidden_size = RAYS_HIDDEN_SIZE
-    latent_size = int(hidden_size / 2)
+    latent_size = RAYS_LATENT_SIZE
+    image_channels = RAYS_IMAGE_CHANNELS
 else:
     image_size = POTENTIAL_IMAGE_SIZE
+    image_channels = POTENTIAL_IMAGE_CHANNELS
     hidden_size = POTENTIAL_HIDDEN_SIZE
-    latent_size = int(hidden_size / 2)
+    latent_size = POTENTIAL_LATENT_SIZE
+    image_channels = POTENTIAL_IMAGE_CHANNELS
 
-vae = ConvVAE(image_dim=image_size, hidden_size=hidden_size, latent_size=latent_size, image_channels=1)
-vae.load_state_dict(torch.load(model_path))
+vae = ConvVAE(image_dim=image_size, hidden_size=hidden_size, latent_size=latent_size, image_channels=image_channels)
+vae.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
 vae.eval()
-
 
 encodings = []
 
-max_samples = 500
+max_samples = 160
 
 for i, idx in enumerate(range(max_samples)):
-    sample = train_dataset.X[idx].unsqueeze(0).float()
+    sample = train_dataset[idx].unsqueeze(0).float()
     mean, log_var = vae.encode(sample)
     sample_encoded = torch.cat((mean, log_var), 0).flatten().tolist()
     encodings.append(sample_encoded)
-
 
 encodings_embedded = TSNE(n_components=2).fit_transform(encodings)
 
--- visualize_reconstruction.py
+++ visualize_reconstruction.py
@@ -7,8 +7,8 @@ import itertools
 
 batch_size = 1
 
-dataset = 'rays'
-model_name = 'rays_VAE__2021-03-09 18:11:30.087612.pt'
+dataset = 'potential'
+model_name = 'potential_VAE__2021-03-10 20_41_23.106999.pt'
 model_path = MODELS_ROOT + model_name
 
 if dataset == 'rays':
@@ -27,17 +27,16 @@ else:
 # ae = ConvVAE(image_dim=image_size, hidden_size=hidden_size, latent_size=latent_size, image_channels=image_channels)
 ae = ConvVAE(image_dim=image_size, hidden_size=hidden_size, latent_size=latent_size, image_channels=image_channels,
              net_size=1)
-ae.load_state_dict(torch.load(model_path))
+ae.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
 ae.eval()
 
 train_dataset = torch.load(DATA_ROOT + 'DATA21.2.18/loaded_data/' + 'training_' + dataset + '.pt')
 test_dataset = torch.load(DATA_ROOT + 'DATA21.2.18/loaded_data/' + 'test_' + dataset + '.pt')
-train_dataset = train_dataset[0, :, :, :].unsqueeze(0)
 train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
 test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
 
-rand_sample_idx = random.randint(0, 500)
-rand_sample = next(itertools.islice(train_loader, rand_sample_idx, None))
+rand_sample_idx = random.randint(0, 40)
+rand_sample = next(itertools.islice(test_loader, rand_sample_idx, None))
 
 rand_sample_prime = ae(rand_sample[0].reshape(1, image_channels, image_size, image_size))[0]
 
