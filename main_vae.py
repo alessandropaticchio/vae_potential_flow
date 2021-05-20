6465b7ac2b6e37f8797eab27d15ce5b7a829e2f8
alessandro.paticchio <alessandro.paticchio@moviri.com>
2021-05-20 15:23:14 +0200

    ready to train on 3 strengths

--- main_vae.py
+++ main_vae.py
@@ -24,7 +24,6 @@ pics_train_dataset, strength_train_dataset = generate_dataset_from_strength(pics
 pics_test_dataset, strength_test_dataset = generate_dataset_from_strength(pics_test_dataset, strength_test_dataset,
                                                                           strengths)
 
-
 train_dataset = StrengthDataset(x=pics_train_dataset, d=strength_train_dataset)
 test_dataset = StrengthDataset(x=pics_test_dataset, d=strength_test_dataset)
 
@@ -35,20 +34,20 @@ test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch
 if dataset == 'rays':
     image_size = RAYS_IMAGE_SIZE
     image_channels = RAYS_IMAGE_CHANNELS
-    # hidden_size = RAYS_HIDDEN_SIZE
-    hidden_size = 8 * 48 * 48
+    hidden_size = RAYS_HIDDEN_SIZE
+    # hidden_size = 8 * 48 * 48
     latent_size = RAYS_LATENT_SIZE
     image_channels = RAYS_IMAGE_CHANNELS
 else:
     image_size = POTENTIAL_IMAGE_SIZE
     image_channels = POTENTIAL_IMAGE_CHANNELS
-    # hidden_size = POTENTIAL_HIDDEN_SIZE
-    hidden_size = 8 * 48 * 48
+    hidden_size = POTENTIAL_HIDDEN_SIZE
+    # hidden_size = 8 * 48 * 48
     latent_size = POTENTIAL_LATENT_SIZE
     image_channels = POTENTIAL_IMAGE_CHANNELS
 
-vae = ConvVAETest(image_dim=image_size, hidden_size=hidden_size, latent_size=latent_size, image_channels=image_channels,
-                net_size=1, conditional=conditional)
+vae = ConvVAE(image_dim=image_size, hidden_size=hidden_size, latent_size=latent_size, image_channels=image_channels,
+              net_size=1, conditional=conditional)
 
 lr = 1e-3
 optimizer = optim.Adam(vae.parameters(), lr=lr, weight_decay=0)
@@ -58,11 +57,8 @@ if dataset == 'potential':
 else:
     power = 4
 
-# from models import weights_init
-# vae.apply(weights_init)
-
 recon_weight = 1.
-kl_weight = 1.
+kl_weight = 4.
 reg_weight = 0.
 
 train_vae(net=vae, train_loader=train_loader, test_loader=test_loader, epochs=epochs, optimizer=optimizer,
